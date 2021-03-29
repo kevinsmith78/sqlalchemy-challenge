08f7ea2c08ef02746404d0516b11ee3b09e11e22
Kevin Smith <kevin.ar.smith@gmail.com>
2021-03-29 17:11:26 -0500

    questions

--- app.py
+++ app.py
@@ -29,12 +29,34 @@ def welcome():
         f"Available Routes:"
         f"Precipitation:/api/v1.0/precipitation"
         f"List of stations:/api/v1.0/stations"
-        f"Temperature:/api/v1.0/stations"
         f"Observations:/api/v1.0/tobs"
         f"Dates:/api/v1.0/<start>/<end>"
     )
 @app.route ("/api/v1.0/precipitation") 
-def precipitation():    
+def precipitation(): 
+#Convert the query results to a dictionary using date as the key and prcp as the value.
+
+
+#Return the JSON representation of your dictionary.
+
+
+@app.route("/api/v1.0/stations")
+#Return a JSON list of stations from the dataset.
+
+@app.route("Observations:/api/v1.0/tobs")
+#Query the dates and temperature observations of the most active station for the last year of data.
+
+
+#Return a JSON list of temperature observations (TOBS) for the previous year.
+
+@app.route ("Dates:/api/v1.0/<start>/<end>")
+#Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
+
+
+#When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
+
+
+#When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive.
     pass   
     
 if __name__ == '__main__':
